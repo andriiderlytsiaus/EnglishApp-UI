d59359680b273e9fda8aae239eab81184a15a402
Andrii Derlytsia <andriiderlytsia@gmail.com>
2024-01-03 20:01:50 -0800

    add basename

--- src/App.js
+++ src/App.js
@@ -7,45 +7,47 @@ import Lesson from './pages/Lesson';
 import SignIn from './pages/SignIn';
 import Register from './pages/Register';
 import CreateLesson from './components/CreateLesson/CreateLesson';
-import './App.css'
-
+import './App.css';
 
 function App() {
   const [userId, setUserId] = useState(null);
-Window.userId = userId;
+  Window.userId = userId;
+
   const handleSignIn = (id) => {
     setUserId(id);
   };
+
   useEffect(() => {
     console.log('User ID in Parent Component:', userId);
   }, [userId]);
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/EnglishApp-UI">
       <header>
-        <h2> EnglishApp</h2> 
+        <h2> EnglishApp</h2>
         <nav>
-        <Link to="/">Home</Link>
-        {Window.userId === null ? (
+          <Link to="/home">Home</Link>
+          {Window.userId === null ? (
             <Link to="/signIn">Dictionary</Link>
           ) : (
             <Link to="/words">Dictionary</Link>
           )}
-       </nav>
-       <Link to="/signIn" id='SignInLink' className='SignInButton'>Account</Link>
-      </header> 
-      <main>  
-      <Routes>
-          <Route index element ={<Home userId={userId}/>} />
-          <Route path="/home" element = {<Home userId={userId} />}/>
-          <Route path="/words" element = {<Words userId={userId}/>}/>
-          <Route path="/lesson/:lessonId" element = {<Lesson />}/>
-          <Route path="/createLesson/:lessonId" element = {<CreateLesson />}/>
-          <Route path="/signIn" element = {<SignIn  onSignIn={handleSignIn}/>}/>
-          <Route path="/register" element = {<Register />}/>
-        </Routes>   
+        </nav>
+        <Link to="/signIn" id='SignInLink' className='SignInButton'>Account</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home userId={userId} />} />
+          <Route path="/home" element={<Home userId={userId} />} />
+          <Route path="/words" element={<Words userId={userId} />} />
+          <Route path="/lesson/:lessonId" element={<Lesson />} />
+          <Route path="/createLesson/:lessonId" element={<CreateLesson />} />
+          <Route path="/signIn" element={<SignIn onSignIn={handleSignIn} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </main>
-      <Footer/>
-    </BrowserRouter>  
+      <Footer />
+    </BrowserRouter>
   );
 }
 
